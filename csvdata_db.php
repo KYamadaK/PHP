70c3e26c9dbc6e974f3e6287515084628b674d1e
Yamada999 <masterhinagiku@gmail.com>
2019-11-04 19:23:40 +0900

    過去の演習スクリプト
    データベースからデータを抽出し、CSV形式にして出力するサンプル

---
+++ csvdata_db.php
@@ -0,0 +1,15 @@
+﻿<?php
+$out = fopen('php://output', 'w');
+$sqliteerror="エラー";
+$pdo = new PDO('sqlite:bb_player.sqlite');
+$results = $pdo->query("select * from player");
+if (!$results) {
+    die('SQL errer.'.$sqliteerror);
+}
+foreach($results as $values){
+	fputcsv($out, array($values[0],$values[1],$values[2]));
+}
+$pdo = null;
+
+fclose($out);
+?>
