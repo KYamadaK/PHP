a7333154dc7d759a15cca8dffff659ea997248d3
yamada999 <masterhinagiku@gmail.com>
2020-03-01 22:00:23 +0900

    条件分岐

--- sample/sample3.php
+++ sample/sample3.php
@@ -1,12 +1,12 @@
 ﻿<?php
-	print "<p>条件分岐</p>";
-	$A = 0;
-	if($A){
-		while(1){
-		}
-	}else{
-		print("無限ループの回避");
-	}
+print "<p>条件分岐</p>";
+$A = 0;
+if($A){
+  while(1){
+  }
+}else{
+  print("無限ループの回避");
+}
 ?>
 <br>
 <a href="sample4.php">sample4</a>
